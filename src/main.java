736c22b80ea73c146fd0c1e6d57dd2008bf4f5b4
Yuliya Tonkikh <uliya_i@bk.ru>
2022-11-20 21:27:28 +0700

    first commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[ ]args) {
+        int ticketPrice = 10686;
+        int numberOfRublasForOneMiles = 20;
+        System.out.println((ticketPrice / numberOfRublasForOneMiles) + "количество милей");
+    }
+}
